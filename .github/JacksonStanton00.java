7f6a366051d176cad719f20989f2712f4bd051ee
s11169125 <70713988+s11169125@users.noreply.github.com>
2022-01-24 09:08:15 -0500

    v0.1

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -1,4 +1,4 @@
-// Challah Byard, Arrays Practice, v0.0, 01/24/22
+// Challah Byard, Arrays Practice, v0.1, 01/24/22
 
 public class JacksonStanton00 {
 
@@ -8,10 +8,18 @@ public class JacksonStanton00 {
         int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
         double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
 
-    
-        
+        for (current_element = 0; current_element < carArr.length(); current_element += 1) {
+            // code to execute
+            //Statement 1 is execute ONCE BEFORE THE LOOP.
+            //Statment 2 is the CODITIONAL for the loop.
+            //Statement 3 is executed EACH TIME THE LOOP RUNS.
+             
         }
 
+        for (i = 0; i < carArr.length(); i++) {
+
+
+        }
 
 
 

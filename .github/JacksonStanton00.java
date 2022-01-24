6309ee80a6446c9c685481a88c8b8ec38bf82984
s11169125 <70713988+s11169125@users.noreply.github.com>
2022-01-24 08:49:42 -0500

    v0.0
    Array Practice

---
+++ .github/JacksonStanton00.java
@@ -0,0 +1,26 @@
+// Challah Byard, Arrays Practice, v0.0, 01/24/22
+
+public class JacksonStanton00 {
+
+    public static void main(String[] args) {
+
+        String[] carArr = {"BMW", "Ferrari 250 GTO", "Porshe 911", "Ford F150 Lighting", "Buick Skylark"};
+        int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
+        double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
+
+    
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+}

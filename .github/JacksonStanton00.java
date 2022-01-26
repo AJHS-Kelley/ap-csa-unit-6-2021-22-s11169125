bdadd2e25ddb8d37f8048013298d70a014bc8839
s11169125 <70713988+s11169125@users.noreply.github.com>
2022-01-26 08:35:53 -0500

    v0.1

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -4,10 +4,9 @@ public class JacksonStanton00 {
 
     public static void main(String[] args) {
 
-        String[] carArr = {"BMW", "Ferrari 250 GTO", "Porshe 911", "Ford F150 Lighting", "Buick Skylark"};
         int[] myGrades = {100, 25, 15, -3, 0, 75, 35};
         double[] myBankAccount = {0.0, -1.25, 3.75, 456.99, 25.25, 12.123456, 3.14};
-
+        /*
         for (current_element = 0; current_element < carArr.length(); current_element += 1) {
             // code to execute
             //Statement 1 is execute ONCE BEFORE THE LOOP.
@@ -15,12 +14,31 @@ public class JacksonStanton00 {
             //Statement 3 is executed EACH TIME THE LOOP RUNS.
              
         }
+         */
+        
 
         for (i = 0; i < carArr.length(); i++) {
+            // System.out.println(i);
+            System.out.println(carArr[i]);
+
+
 
 
         }
+       
+        
+        String[] carArr = {"BMW", "Ferrari 250 GTO", "Porshe 911", "Ford F150 Lighting", "Buick Skylark"};
+        System.out.println(carArr[3]);
+        // ONCE CREATED YOU CANNOT CHANGE THE SIZE OD AN ARRAY. IT IS FORBIDDEN!
+        CcarArr[0] = "Volkswagen Passat"; 
 
+        for (String eachElement : carArr) {
+            // Code to execute.
+            System.out.println(eachElement);
+        
+        }
+    }
+}
 
 
 
